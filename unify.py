99de4a1e783609d9ba261ebe695e4958a7837b38
170joeychua <170joeychua@gmail.com>
2021-10-01 12:25:29 +0800

    add admin login page and route

--- unify.py
+++ unify.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# admin route
+@app.route('/admin-only/login/')
+def admin():
+    return render_template('admin/admin.html')
+
 if __name__ == "__main__":
     # Error will be displayed on web page 
     app.run(debug=True)
