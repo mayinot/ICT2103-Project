3a8d62b94b9c9b46e3bb913d09ac5133debad219
alphonsekoh <alphonsekoh@gmail.com>
2021-10-06 17:39:58 +0800

    Added template random chart

--- unify.py
+++ unify.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/dashboard.html')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == "__main__":
     # Error will be displayed on web page 
     app.run(debug=True)
