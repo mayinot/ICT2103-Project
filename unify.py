ecab1bcd0d228076699374ce8f353b8feb7439d5
unknown <yujing1314276@gmail.com>
2021-10-20 10:23:09 +0800

    update page style to flask

--- unify.py
+++ unify.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 # index route 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def adminDashboard():
+    return render_template('adminDashBoard.html')
+
+# index route 
+@app.route('/data')
+def data():
+    return render_template('data.html')
+
+@app.route('/adminEdit')
+def adminEdit():
+    return render_template('adminEdit.html')
 
 if __name__ == "__main__":
     # Error will be displayed on web page 
