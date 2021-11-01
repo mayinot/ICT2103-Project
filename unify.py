31a6de33d291d8fa2e7feb7ed92ed7849917e862
170joeychua <170joeychua@gmail.com>
2021-11-01 21:52:48 +0800

    Update index to create dynamic dropdown
    add dynamic dropdown
    disable category dropdown if university is not selected

--- unify.py
+++ unify.py
@@ -1,36 +1,78 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
 from mysql import connector
 import mysql.connector
 from Credentials import constants
 
 conn = mysql.connector.connect(host=constants.HOST,
-        database=constants.DATABASE,
-        user=constants.USER,
-        password=constants.PASSWORD
-        )
+                               database=constants.DATABASE,
+                               user=constants.USER,
+                               password=constants.PASSWORD
+                               )
 
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-# index route 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-# dashboard routing 
+# index route
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    conn = mysql.connector.connect(host=constants.HOST,
+                                   port=constants.PORT,
+                                   database=constants.DATABASE,
+                                   user=constants.USER,
+                                   password=constants.PASSWORD
+                                   )
+    cur = conn.cursor()
+    query = cur.execute("""SELECT UniName
+                 FROM unify_db.University; """)
+    cur.execute(query)
+    uniinfo = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return render_template('index.html', uniinfo=uniinfo)
+
+# Get the category according to the university
+@app.route('/<getCat>')
+def categoryByUniversity(getCat):
+    conn = mysql.connector.connect(host=constants.HOST,
+                                port=constants.PORT,
+                                database=constants.DATABASE,
+                                user=constants.USER,
+                                password=constants.PASSWORD
+                                )
+    cur = conn.cursor()
+    result = cur.execute("""SELECT DISTINCT Ca.CategoryName
+                        FROM unify_db.Category Ca, unify_db.FacultyCategory FC, unify_db.Faculty F, unify_db.Courses C
+                     WHERE Ca.CategoryID = FC.CategoryID
+                     AND FC.FacultyID = F.FacultyID
+                     AND C.FacultyID = F.FacultyID
+                     AND C.UniName = %s
+                     ;""", [getCat])
+
+    category = cur.fetchall()
+    categoryArray = []
+    for row in category:
+        categoryObj = {
+            'id': row[0],
+            'name': row[0]
+        }
+        categoryArray.append(categoryObj)
+    return jsonify({'categoryList' : categoryArray})
+
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html') 
+    return render_template('dashboard.html')
 
-# courses route 
-@app.route('/courses', methods=['GET','POST'])
+# courses route
+@app.route('/courses', methods=['GET', 'POST'])
 def courses():
     conn = mysql.connector.connect(host=constants.HOST,
-        port=constants.PORT,
-        database=constants.DATABASE,
-        user=constants.USER,
-        password=constants.PASSWORD
-        )
+                                   port=constants.PORT,
+                                   database=constants.DATABASE,
+                                   user=constants.USER,
+                                   password=constants.PASSWORD
+                                   )
     cur = conn.cursor()
     # Select all courses for courses card
     result = cur.execute("""SELECT C.CourseName, C.CourseDesc, C.CourseURL, IFNULL(NULLIF(CAST(C.AvgGradPay AS char), "0"), "N/A") as AvgGradPay, U.UniImage, F.FacultyName, C.UniName
@@ -46,7 +88,7 @@ def courses():
     result = cur.execute("""SELECT UniName
                     FROM unify_db.University; """)
     uniinfo = cur.fetchall()
-    
+
     if request.method == 'POST':
         UniList = request.form.getlist('UniFilter')
         category = request.form.get('category')
@@ -58,7 +100,7 @@ def courses():
         UNI_list = str(tuple([key for key in UniList])).replace(',)', ')')
         print(UNI_list)
         print(category)
-        query ="""SELECT C.CourseName, C.CourseDesc, C.CourseURL, IFNULL(NULLIF(CAST(C.AvgGradPay AS char), "0"), "N/A") as AvgGradPay, U.UniImage, F.FacultyName, C.UniName 
+        query = """SELECT C.CourseName, C.CourseDesc, C.CourseURL, IFNULL(NULLIF(CAST(C.AvgGradPay AS char), "0"), "N/A") as AvgGradPay, U.UniImage, F.FacultyName, C.UniName 
         FROM unify_db.Courses C, unify_db.University U, unify_db.Faculty F,  unify_db.Category Ca, unify_db.FacultyCategory FC
         WHERE C.UniName = U.UniName
         AND C.FacultyID = F.FacultyID
@@ -68,56 +110,64 @@ def courses():
         AND C.AvgGradPay >= %s
         AND C.AvgGradPay <= %s
         AND C.UniName IN {UNI_list};""".format(UNI_list=UNI_list)
-        result= cur.execute(query, (category, FROMsalary, TOsalary))
+        result = cur.execute(query, (category, FROMsalary, TOsalary))
         coursesinfo = cur.fetchall()
     cur.close()
     conn.close()
     return render_template('courses.html', coursesinfo=coursesinfo, categoryinfo=categoryinfo, uniinfo=uniinfo)
 
 # admin route (create courses)
+
+
 @app.route('/addcourses')
 def addcourses():
     return render_template('addcourses.html')
 
 # admin route (create courses)
-@app.route('/editcourses', methods=['GET','POST'])
+
+
+@app.route('/editcourses', methods=['GET', 'POST'])
 def editcourses():
     conn = mysql.connector.connect(host=constants.HOST,
-        port=constants.PORT,
-        database=constants.DATABASE,
-        user=constants.USER,
-        password=constants.PASSWORD
-        )
+                                   port=constants.PORT,
+                                   database=constants.DATABASE,
+                                   user=constants.USER,
+                                   password=constants.PASSWORD
+                                   )
     cur = conn.cursor()
     if request.method == 'POST':
         CourseID = request.form.get('CourseId')
         print(CourseID)
-        query ="""SELECT  C.CourseName, C.CourseDesc, C.CourseURL, C.AvgGradPay, C.CourseID
+        query = """SELECT  C.CourseName, C.CourseDesc, C.CourseURL, C.AvgGradPay, C.CourseID
         FROM unify_db.Courses C
         WHERE C.CourseID = %s """
-        result= cur.execute(query, (CourseID,))
+        result = cur.execute(query, (CourseID,))
         Editcoursesinfo = cur.fetchone()
         print(Editcoursesinfo)
     cur.close()
     conn.close()
-    return render_template('editcourses.html', Editcoursesinfo =Editcoursesinfo)
-  
+    return render_template('editcourses.html', Editcoursesinfo=Editcoursesinfo)
+
 # admin route
+
+
 @app.route('/adminLogin/')
 def admin():
     return render_template('admin/adminLogin.html')
+
 
 @app.route('/adminDash')
 def adminDash():
     return render_template('admin/adminDashBoard.html')
 
+
 @app.route('/adminViewData')
 def adminViewData():
     conn = mysql.connector.connect(host=constants.HOST,
-        database=constants.DATABASE,
-        user=constants.USER,
-        password=constants.PASSWORD
-        )
+                                   database=constants.DATABASE,
+                                   user=constants.USER,
+                                   password=constants.PASSWORD
+                                   )
     cur = conn.cursor()
     cur.execute("""SELECT C.CourseID,C.UniName,C.CourseName,
     G.Poly10thPerc,G.Poly90thPerc,G.Alevel10thPerc,G.Alevel90thPerc,
@@ -128,40 +178,43 @@ def adminViewData():
     data = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('admin/adminViewData.html',data = data)
+    return render_template('admin/adminViewData.html', data=data)
+
 
 @app.route('/adminEditData/<Course_ID>', methods=['GET', 'POST'])
 def adminEditData(Course_ID):
     if(request.method == 'GET'):
         print(Course_ID)
         conn = mysql.connector.connect(host=constants.HOST,
-            database=constants.DATABASE,
-            user=constants.USER,
-            password=constants.PASSWORD
-            )
+                                       database=constants.DATABASE,
+                                       user=constants.USER,
+                                       password=constants.PASSWORD
+                                       )
         cur = conn.cursor()
         cur.execute("""SELECT C.CourseName,G.Poly10thPerc,G.Poly90thPerc,G.Alevel10thPerc,G.Alevel90thPerc,intake,C.AvgGradPay 
         FROM unify_db.Courses C, unify_db.GradeProfile G 
-        WHERE C.CourseID = %s """,(Course_ID))
-        # query = """SELECT C.CourseName,G.Poly10thPerc,G.Poly90thPerc,G.Alevel10thPerc,G.Alevel90thPerc,intake,C.AvgGradPay FROM unify_db.Courses C, unify_db.GradeProfile G 
+        WHERE C.CourseID = %s """, (Course_ID))
+        # query = """SELECT C.CourseName,G.Poly10thPerc,G.Poly90thPerc,G.Alevel10thPerc,G.Alevel90thPerc,intake,C.AvgGradPay FROM unify_db.Courses C, unify_db.GradeProfile G
         # # WHERE C.CourseID = %s """.format(Course_ID)
         # cur.execute(query)
         dataToEdit = cur.fetchall()
         print(dataToEdit)
         cur.close()
         conn.close()
-        return render_template('admin/adminEditData.html',dataToEdit = dataToEdit)
+        return render_template('admin/adminEditData.html', dataToEdit=dataToEdit)
     # else:
-    
+
 # admin route
-@app.route('/SuccessfulEdit', methods=['GET','POST'])
+
+
+@app.route('/SuccessfulEdit', methods=['GET', 'POST'])
 def SuccessfulEdit():
     conn = mysql.connector.connect(host=constants.HOST,
-        port=constants.PORT,
-        database=constants.DATABASE,
-        user=constants.USER,
-        password=constants.PASSWORD
-        )
+                                   port=constants.PORT,
+                                   database=constants.DATABASE,
+                                   user=constants.USER,
+                                   password=constants.PASSWORD
+                                   )
     cur = conn.cursor()
     if request.method == 'POST':
         CourseID = request.form.get('CourseId')
@@ -171,36 +224,37 @@ def SuccessfulEdit():
         CourseDesc = request.form.get('CourseDesc')
         cur.execute("""  UPDATE unify_db.Courses C
                     SET C.CourseName = %s, C.CourseDesc = %s, C.CourseURL = %s, C.AvgGradPay = %s
-                    WHERE C.CourseID = %s""", 
-               (CourseName, CourseDesc, CourseURL, CourseSalary, CourseID,))
+                    WHERE C.CourseID = %s""",
+                    (CourseName, CourseDesc, CourseURL, CourseSalary, CourseID,))
         conn.commit()
     cur.close()
     conn.close()
-    
+
     return render_template('admin/SuccessfulEdit.html')
 
 
 # admin route
-@app.route('/deletecourses', methods=['GET','POST'])
+@app.route('/deletecourses', methods=['GET', 'POST'])
 def deletecourses():
     conn = mysql.connector.connect(host=constants.HOST,
-        port=constants.PORT,
-        database=constants.DATABASE,
-        user=constants.USER,
-        password=constants.PASSWORD
-        )
+                                   port=constants.PORT,
+                                   database=constants.DATABASE,
+                                   user=constants.USER,
+                                   password=constants.PASSWORD
+                                   )
     cur = conn.cursor()
     if request.method == 'POST':
         CourseID = request.form.get('CourseId')
         print(CourseID)
         cur.execute("""DELETE FROM unify_db.Courses C 
-                        WHERE C.CourseID =%s """, 
-               (CourseID,))
+                        WHERE C.CourseID =%s """,
+                    (CourseID,))
         conn.commit()
     cur.close()
-    conn.close()    
+    conn.close()
     return render_template('deletecourses.html')
 
+
 if __name__ == "__main__":
-    # Error will be displayed on web page 
+    # Error will be displayed on web page
     app.run(debug=True)
