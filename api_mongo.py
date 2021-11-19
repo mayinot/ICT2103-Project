cccfb831ceeb30f560cdb2c159ad7dcc69427fe7
alphonsekoh <alphonsekoh@gmail.com>
2021-11-19 15:57:20 +0800

    fix: query data for db

--- api_mongo.py
+++ api_mongo.py
@@ -5,23 +5,26 @@ import pymongo
 
 app = Flask(__name__)
 
-
-def unify_db():
-    # connecting to mongo
-    app.config["MONGO_URI"] = constants.MONGO_CONNECT
-    mongo = PyMongo(app)
-    # getting the db and disable the SSL certificate for UNIX developers
-    db = mongo.init_app(app, tlsAllowInvalidCertificates=True)
-
-    return db
+# connecting to mongo
+app.config["MONGO_URI"] = constants.MONGO_CONNECT
+mongo = PyMongo(app)
+# getting the db and disable the SSL certificate for UNIX developers
+mongo.init_app(app, tlsAllowInvalidCertificates=True)
 
 
-def fetch_Courses():
-    db = unify_db()
-    courses = db.courses
+def fetch_Courses() -> object:
+    '''
+
+    Args:
+        None
+    Returns:
+        string
+    '''
+
+    courses = mongo.db.courses
     cursor = courses.find()
     return cursor
 
 
 if __name__ == "__main__":
-    fetch_Courses()
+    print(fetch_Courses())
