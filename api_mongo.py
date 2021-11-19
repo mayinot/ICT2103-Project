559c936937a9f993373f2acd246936ab94a503d4
alphonsekoh <alphonsekoh@gmail.com>
2021-11-19 15:15:15 +0800

    fix: SSL certificate disable for UNIX

--- api_mongo.py
+++ api_mongo.py
@@ -7,18 +7,20 @@ app = Flask(__name__)
 
 
 def unify_db():
-    #connecting to mongo
-    client = pymongo.MongoClient(constants.MONGO_CONNECT)
-    #getting the db
-    db = client.unify_db
+    # connecting to mongo
+    app.config["MONGO_URI"] = constants.MONGO_CONNECT
+    mongo = PyMongo(app)
+    # getting the db and disable the SSL certificate for UNIX developers
+    db = mongo.init_app(app, tlsAllowInvalidCertificates=True)
+
     return db
+
 
 def fetch_Courses():
     db = unify_db()
     courses = db.courses
     cursor = courses.find()
     return cursor
-
 
 
 if __name__ == "__main__":
