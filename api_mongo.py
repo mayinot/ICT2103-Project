17931488176ccaf0cf89a9543d17288bc57343a1
170joeychua <170joeychua@gmail.com>
2021-11-20 05:48:35 +0800

    Merge branch 'master' of https://github.com/mayinot/ICT2103-Project into joey

--- api_mongo.py
+++ api_mongo.py
@@ -5,17 +5,23 @@ import pymongo
 
 app = Flask(__name__)
 
+# connecting to mongo
+app.config["MONGO_URI"] = constants.MONGO_CONNECT
+mongo = PyMongo(app)
+# getting the db and disable the SSL certificate for UNIX developers
+mongo.init_app(app, tlsAllowInvalidCertificates=True)
 
-def unify_db():
-    #connecting to mongo
-    client = pymongo.MongoClient(constants.MONGO_CONNECT)
-    #getting the db
-    db = client.unify_db
-    return db
 
-def fetch_Courses():
-    db = unify_db()
-    courses = db.courses
+def fetch_Courses() -> object:
+    '''
+
+    Args:
+        None
+    Returns:
+        string
+    '''
+
+    courses = mongo.db.courses
     cursor = courses.find()
     return cursor
 
@@ -35,4 +41,4 @@ def fetch_CategoryNames():
     return join_collection
 
 if __name__ == "__main__":
-    fetch_Courses()
+    print(fetch_Courses())
