3f26c4612c4395edc850797e5ebe61454067351a
170joeychua <170joeychua@gmail.com>
2021-11-20 19:06:21 +0800

    fix index no sql

--- api_mongo.py
+++ api_mongo.py
@@ -16,9 +16,7 @@ mongo.init_app(app, tlsAllowInvalidCertificates=True)
 app.config["MONGO_URI"] = constants.MONGO_CONNECT
 mongo = PyMongo(app)
 # getting the db and disable the SSL certificate for UNIX developers
-db= mongo.init_app(app, tlsAllowInvalidCertificates=True)
-
-
+db = mongo.init_app(app, tlsAllowInvalidCertificates=True)
 
 
 def fetch_Courses() -> object:
@@ -33,6 +31,7 @@ def fetch_Courses() -> object:
     cursor = courses.find()
     return cursor
 
+
 def fetch_Uninames():
     '''
     Query to get all the universities
@@ -40,17 +39,19 @@ def fetch_Uninames():
     Args:
         connection_string (MySQLConnection): The database location mysql connector
     Returns:
-        uniFilter (list): a list of tuples representing the queried payload   
+        uniFilter (list): a list of tuples representing the queried payload
     '''
     univeristy = mongo.db.courses
     uniFilter = univeristy.distinct("University.UniName")
     return uniFilter
 
+
 def fetch_CategoryNames():
     category = mongo.db.category
     courses = mongo.db.courses
-    cursor = category.distinct( "CategoryName")
+    cursor = category.distinct("Category.CategoryName")
     return cursor
+
 
 def fetch_CategoryNames(getUniCat):
     '''
@@ -59,25 +60,26 @@ def fetch_CategoryNames(getUniCat):
     Args:
         getUniCat: get the selected university
     Returns:
-        list: a list of tuples representing the queried payload 
+        list: a list of tuples representing the queried payload
     '''
     courses = mongo.db.courses
-    category = mongo.db.category
-    category_name = category.distinct("CategoryName")
-    join_collection = courses.aggregate([{"$lookup": { "from": "category", "localField": "Faculty.CategoryID", "foreignField": "CategoryID", "as": "Category_Info" }}, { "$match": { "Category_Info": { "$elemMatch": { "University.UniName" : { "$in": getUniCat }}  }}}])
-    return join_collection
+    categoryList = courses.aggregate([{"$lookup": {"from": "category", "localField": "Faculty.CategoryID", "foreignField": "CategoryID", "as": "Category_Info"}},
+                                      {"$match": {"University.UniName": getUniCat}}])
+    return list(categoryList)
+
 
 def filter_Course(UniList, category_name, FROMsalary, TOsalary):
     if TOsalary < FROMsalary:
-            flash('To Salary cannot be more than From Salary!')
-            redirect(url_for('courses'))
+        flash('To Salary cannot be more than From Salary!')
+        redirect(url_for('courses'))
     category = mongo.db.category
     courses = mongo.db.courses
-    join_collection = courses.aggregate([{"$lookup": { "from": "category", "localField": "Faculty.CategoryID", "foreignField": "CategoryID", "as": "Category_Info" }}, 
-    { "$match": { "Category_Info": { "$elemMatch": { "CategoryName": category_name }}, "AvgGradPay": { "$gte": FROMsalary, "$lte": TOsalary}, "University.UniName" : { "$in": UniList } }}])
+    join_collection = courses.aggregate([{"$lookup": {"from": "category", "localField": "Faculty.CategoryID", "foreignField": "CategoryID", "as": "Category_Info"}},
+                                         {"$match": {"Category_Info": {"$elemMatch": {"CategoryName": category_name}}, "AvgGradPay": {"$gte": FROMsalary, "$lte": TOsalary}, "University.UniName": {"$in": UniList}}}])
     return join_collection
-    
+
+
 if __name__ == "__main__":
     fetch_Courses()
     filter_Course(UniList, category_name, FROMsalary, TOsalary)
-
+    fetch_CategoryNames("Singapore Institute of Technology")
