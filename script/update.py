7aa779a8dd5faef315599488dec3109730e9b6ce
alphonsekoh <alphonsekoh@gmail.com>
2021-11-20 17:01:45 +0800

    fix: change grad salary of NA to 0 for mongo sorting

--- script/update.py
+++ script/update.py
@@ -13,18 +13,8 @@ Do tweak the variables in your accordance
 
 
 def emergency_update():
-    query = {"University": {
-        "UniName": "Singapore Institute of Technology",
-        "UniAbb": "SIT",
-        "UniDesc": "Singapore Institute of Technology (SIT) is Singapore’s university of applied learning. SIT’s vision is to be a leader in innovative learning by integrating learning, industry and community. Its mission is to nurture and develop individuals who build on their interests and talents to impact society in meaningful ways.",
-        "UniImage": "https://digitalsenior.sg/wp-content/uploads/2013/10/SIT-logo.jpg"
-    }}
-    update_dataset = {"$set": {"GradeProfile": {
-        "Poly10thPerc": "N.A",
-        "Poly90thPerc": "N.A",
-        "Alevel10thPerc": "N.A",
-        "Alevel90thPerc": "N.A"
-    }}}
+    query = {"AvgGradPay": "N.A"}
+    update_dataset = {"$set": {"AvgGradPay": "0"}}
     update_data = api_mongo.mongo.db.courses
     update_data.update_many(query, update_dataset)
 
