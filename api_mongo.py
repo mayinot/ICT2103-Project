6233443e66a852e5fb09425712fb69920a2d1e15
yaoyujing <yujing1314276@gmail.com>
2021-11-24 14:08:37 +0800

    minor update on adminaddcourse route

--- api_mongo.py
+++ api_mongo.py
@@ -85,10 +85,16 @@ def filter_Course(UniList, category_name, FROMsalary, TOsalary):
     return join_collection
 
 def insert_Course():
-    insertInfo = {"CourseID":request.form.get('courseID'),"University":{'Uniname':request.form.get('university')},"CourseName":request.form.get('course'),"CourseDesc":request.form.get('description'),
-        "GradeProfile":{'Poly10thPerc':request.form.get('poly10')},"GradeProfile":{'Poly90thPerc':request.form.get('poly90')},"GradeProfile":{'Alevel10thPerc':request.form.get('Alevel10')},"GradeProfile":{'Alevel90thPerc':request.form.get('Alevel90')},"Intake":request.form.get('intake'),"AvgGradPay":request.form.get('avgpay'),
+    insertInfo = {"CourseID":request.form.get('courseID'),
+    "University":{'Uniname':request.form.get('university')},
+    "CourseName":request.form.get('course'),"CourseDesc":request.form.get('description'),
+        "GradeProfile":{'Poly10thPerc':request.form.get('poly10')},
+        "GradeProfile":{'Poly90thPerc':request.form.get('poly90')},
+        "GradeProfile":{'Alevel10thPerc':request.form.get('Alevel10')},
+        "GradeProfile":{'Alevel90thPerc':request.form.get('Alevel90')},
+        "Intake":request.form.get('intake'),
+        "AvgGradPay":request.form.get('avgpay'),
          }
-    print(mongo.db)
     mongo.db.courses.insert(insertInfo)
 
 def delete_Course(CourseID):
